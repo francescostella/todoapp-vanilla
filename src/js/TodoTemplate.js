1580fd36afcc1905364c97514fd09d87ff2c4193
Francesco <1682099+francescostella@users.noreply.github.com>
2022-03-16 16:43:52 +0100

    style: clean up

--- src/js/TodoTemplate.js
+++ src/js/TodoTemplate.js
@@ -1,34 +1,7 @@
 export default function TodoTemplate(todo) {
   const template = document.getElementById('template--todo-item');
-  console.log(`🚀 ~ TodoTemplate ~ template`, template)
-
   const $todoItem = template.content.cloneNode(true);
+
   $todoItem.querySelector('.list__description').textContent = todo.value;
   return $todoItem;
-
-
-  // const item = document.createElement('li');
-  // item.classList.add('list__item');
-
-  // const label = document.createElement('label');
-  // label.classList.add('list__label');
-
-  // const checkbox = document.createElement('input');
-  // checkbox.classList.add('list__checkbox');
-  // checkbox.type = 'checkbox';
-
-  // const description = document.createElement('span');
-  // description.classList.add('list__description');
-  // description.textContent = todo.value;
-
-  // const button = document.createElement('button')
-  // button.classList.add('list__delete');
-  // button.textContent = '❌';
-
-  // label.appendChild(checkbox);
-  // label.appendChild(description);
-  // item.appendChild(label);
-  // item.appendChild(button);
-
-  // return item;
 }
