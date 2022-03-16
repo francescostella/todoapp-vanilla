88f996c97413cc3951642d26dbc6520baa598ada
Francesco <1682099+francescostella@users.noreply.github.com>
2022-03-16 16:45:43 +0100

    docs: adding comments to TodoApp class

--- src/js/TodoApp.js
+++ src/js/TodoApp.js
@@ -2,24 +2,24 @@ import TodoService from './TodoService';
 import TodoTemplate from './TodoTemplate';
 
 export default class TodoApp {
-  constructor() {
+  constructor() {}
+
+  init() {
+    // Get HTML element references
     this.todoService = new TodoService();
     this.$todoList = document.querySelector('.list');
     this.$newTodoInput = document.querySelector('.add-form__input');
     this.$addForm = document.querySelector('.add-form');
 
+    // Initialization sequence
     this.renderTodos();
     this.bind();
-  }
-
-  init() {
-    console.log('HEY HEY HEY');
   }
 
   bind() {
     this.$addForm.addEventListener('submit', event => {
       event.preventDefault();
-      
+
       const newValue = this.$newTodoInput.value;
 
       if (newValue) {
