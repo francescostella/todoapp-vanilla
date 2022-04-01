e89588fac1c0b7e183d84ddb9b2a0c3d1ca89ec3
Francesco <1682099+francescostella@users.noreply.github.com>
2022-04-01 16:57:25 +0200

    docs: add jsdocs TodoService

--- src/js/TodoService.js
+++ src/js/TodoService.js
@@ -1,24 +1,56 @@
 import Todo from './Todo';
 
+/**
+ * Utilities
+ */
 let _lastId = 0;
 
 function generateTodoID() {
   return _lastId += 1;
 }
+
+/**
+ *  TodoService
+ *  service delegated to add, count, clear and get all Todos
+ */
 export default class TodoService {
   _todos = [];
 
+  /**
+   * Create a TodoService
+   * if `todos` param is passed, it will be initialized 
+   * with the items passed in it.
+   * @constructor
+   * @param {Todo[]} todos - List of todos
+   */
   constructor(todos) {
     if (todos) {
       todos.forEach(todo => this.add(todo));
     }
   }
 
+  /**
+   * Checks if the todos list is empty
+   * @returns {boolean} true if todos list is empty
+   */
+  isEmpty() {
+    return this._todos.length === 0;
+  }
+
+  /**
+   * Add a todo
+   * @param {string} value - will be used as value of the new Todo
+   */
   add(value) {
     let newTodo = new Todo(generateTodoID(), value)
     this._todos.unshift(newTodo);
   }
 
+  /**
+   * Delete a todo by id
+   * @param {string} id - matching the todo to delete
+   * @returns {Todo} deleted Todo
+   */
   delete(id) {
     let todoToRemove;
 
@@ -34,29 +66,25 @@ export default class TodoService {
     return todoToRemove;
   }
 
+  /**
+   * Get the todo matching the id passed
+   * @param {string} id - matching the todo that will be returned
+   * @returns {Todo} return a single todo
+   */
   getTodoByID(id) {
     return this._todos.find(todo => todo.id === parseInt(id));
   }
 
-  clearCompleted() {
-    this._todos = this._todos.filter(todo => !todo.completed);
-  }
-
-  isEmpty() {
-    return this._todos.length === 0;
-  }
-
-  moveToTop(id) {
-    const index = this._todos.findIndex(todo => todo.id === id);
-
-    if (index >= 0) {
-      const removeTodo = this._todos.splice(index, 1);
-      this._todos.unshift(...removeTodo);
-    }
-  }
-
+  /**
+   * Get the count of the different todos states.
+   * @returns {{
+   *  active: number,
+   *  completed: number,
+   *  total: number
+   * }} an object with `active`, `completed`, and `total` todos
+   */
   getCountTodos() {
-    const todos = {
+    const counters = {
       active: 0,
       completed: 0,
       total: 0,
@@ -64,14 +92,35 @@ export default class TodoService {
 
     this._todos.forEach(todo => {
       if (todo.completed) {
-        todos.completed++;
+        counters.completed++;
       } else {
-        todos.active++;
+        counters.active++;
       }
 
-      todos.total++;
+      counters.total++;
     });
 
-    return todos;
+    return counters;
+  }
+
+  /**
+   * Clear all the todos marked as 'completed'
+   */
+  clearCompleted() {
+    this._todos = this._todos.filter(todo => !todo.completed);
+  }
+
+  /**
+   * Move the todo matching the `id` passed as 
+   * first element of the `todos` list
+   * @param {string} id - matching the todo to move
+   */
+  moveToTop(id) {
+    const index = this._todos.findIndex(todo => todo.id === id);
+
+    if (index >= 0) {
+      const removeTodo = this._todos.splice(index, 1);
+      this._todos.unshift(...removeTodo);
+    }
   }
 }
